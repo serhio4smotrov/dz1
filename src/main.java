77bce2e8bff46074ad3689e7f3fc82d479b6aecd
sergey <ser19061995@gmail.com>
2022-07-24 13:36:28 +0300

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("hello1");
+        System.out.println("hello2");
+        System.out.println("hello3");
+        System.out.println("hello4");
+    }
+}
