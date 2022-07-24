6b2e058380babab38f169faed988cf6f5cf77b9b
sergey <ser19061995@gmail.com>
2022-07-24 14:44:16 +0300

    выполнил дз1 еще раз

--- src/main1.java
+++ src/main1.java
@@ -1,8 +1,8 @@
-public class main {
+public class main1 {
     public static void main(String[] args) {
         System.out.println("hello1");
-        System.out.println("hello2");
         System.out.println("hello3");
+        System.out.println("hello4");
         System.out.println("hello4");
     }
 }
